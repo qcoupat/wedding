e0a3c96f6bea9db7fadf7d8235335105a07adfa6
Quentin Coupat <quentincoupat@gmail.com>
2021-03-30 22:08:09 +0200

    FInalisation des canvas

--- assets/js/canva.js
+++ assets/js/canva.js
@@ -9,3 +9,13 @@ ctx.lineTo(0, 450);
 ctx.closePath();
 ctx.stroke();
 
+// ************************* Canvas *******************
+var canvas = document.querySelector("#zoneCanvas1");
+var ctx     = canvas.getContext("2d");
+ctx.fillStyle = 'rgb(229, 226, 225)'
+ctx.lineWidth = 0.2;
+ctx.beginPath();
+ctx.moveTo(1100, 0);
+ctx.lineTo(0, 450);
+ctx.closePath();
+ctx.stroke();
