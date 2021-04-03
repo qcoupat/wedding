a94b3292fcc76c2ff778d1483acfe302f1b070ed
Quentin Coupat <quentincoupat@gmail.com>
2021-04-03 11:46:32 +0200

    correction bug scroll

--- assets/js/wedding.js
+++ assets/js/wedding.js
@@ -30,15 +30,4 @@ tRebour=setTimeout ("Rebour();", 1000);
 }
 Rebour();
 
-// ******************** Redirection de la mavbar sur la page *********************
-
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        let $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
 
